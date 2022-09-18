c8d6ce902a9df025d6b4a1696fbd32814bd444c1
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-18 22:06:57 +0400

    UserManager

--- src/main/java/manager/UserManager.java
+++ src/main/java/manager/UserManager.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserManager {
-    private Connection connection = DBConnectionProvider.getInstance().getConnection();
+
+    private final Connection connection = DBConnectionProvider.getInstance().getConnection();
 
     public void add(User user) {
         String sql = "insert into user(name,surname,email,password) values(?,?,?,?)";
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, user.getName());
             ps.setString(2, user.getSurname());
             ps.setString(3, user.getEmail());
@@ -32,8 +32,7 @@ public class UserManager {
     public List<User> getAll() {
         String sql = "select * from user";
         List<User> users = new ArrayList<>();
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ResultSet resultSet = ps.executeQuery();
             while (resultSet.next()) {
                 users.add(getUserFromResultSet(resultSet));
@@ -45,25 +44,9 @@ public class UserManager {
     }
 
     public User getById(int id) {
-        String sql = "select * from user where id = " + id;
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ResultSet resultSet = ps.executeQuery();
-            if (resultSet.next()) {
-                return getUserFromResultSet(resultSet);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public User getUserByEmailAndPassword(String email, String password) {
-        String sql = "select * from user where email=? and password=?";
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setString(1, email);
-            ps.setString(2, password);
+        String sql = "select * from user where id = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, id);
             ResultSet resultSet = ps.executeQuery();
             if (resultSet.next()) {
                 return getUserFromResultSet(resultSet);
@@ -76,8 +59,7 @@ public class UserManager {
 
     public User getUserByEmail(String email) {
         String sql = "select * from user where email=?";
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, email);
             ResultSet resultSet = ps.executeQuery();
             if (resultSet.next()) {
@@ -89,19 +71,8 @@ public class UserManager {
         return null;
     }
 
-    private User getUserFromResultSet(ResultSet resultSet) throws SQLException {
-        return User.builder()
-                .id(resultSet.getInt("id"))
-                .name(resultSet.getString("name"))
-                .surname(resultSet.getString("surname"))
-                .email(resultSet.getString("email"))
-                .password(resultSet.getString("password"))
-                .build();
-    }
-
     public void deleteUserById(int id) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("delete from user where id =?");
+        try (PreparedStatement preparedStatement = connection.prepareStatement("delete from user where id =?")) {
             preparedStatement.setInt(1, id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -111,8 +82,7 @@ public class UserManager {
 
     public void edit(User user) {
         String sql = "update user set name=?,surname=?,email=?,password=? where id=?";
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, user.getName());
             ps.setString(2, user.getSurname());
             ps.setString(3, user.getEmail());
@@ -122,5 +92,15 @@ public class UserManager {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private User getUserFromResultSet(ResultSet resultSet) throws SQLException {
+        return User.builder()
+                .id(resultSet.getInt("id"))
+                .name(resultSet.getString("name"))
+                .surname(resultSet.getString("surname"))
+                .email(resultSet.getString("email"))
+                .password(resultSet.getString("password"))
+                .build();
     }
 }
