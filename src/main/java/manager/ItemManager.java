e465d014a5de21e7af497d77e1f3eeda4359fa03
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-18 22:11:07 +0400

    ItemManager

--- src/main/java/manager/ItemManager.java
+++ src/main/java/manager/ItemManager.java
@@ -1,23 +1,21 @@
 package manager;
 
 import db.DBConnectionProvider;
-import model.Category;
 import model.Item;
-import model.User;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ItemManager {
-    private Connection connection = DBConnectionProvider.getInstance().getConnection();
-    private CategoryManager categoryManager = new CategoryManager();
-    private UserManager userManager = new UserManager();
+
+    private final Connection connection = DBConnectionProvider.getInstance().getConnection();
+    private final CategoryManager categoryManager = new CategoryManager();
+    private final UserManager userManager = new UserManager();
 
     public void add(Item item) {
         String sql = "insert into item(title,price,category_id,pic_url,user_id) values(?,?,?,?,?)";
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, item.getTitle());
             ps.setDouble(2, item.getPrice());
             ps.setInt(3, item.getCategory().getId());
@@ -37,8 +35,7 @@ public class ItemManager {
     public List<Item> getAll() {
         String sql = "select * from item order by id DESC limit 20";
         List<Item> items = new ArrayList<>();
-        try {
-            Statement statement = connection.createStatement();
+        try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()) {
                 items.add(getItemFromResultSet(resultSet));
@@ -51,8 +48,7 @@ public class ItemManager {
 
     public Item getById(int id) {
         String sql = "select * from item where id=" + id;
-        try {
-            Statement statement = connection.createStatement();
+        try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(sql);
             if (resultSet.next()) {
                 return getItemFromResultSet(resultSet);
@@ -64,77 +60,18 @@ public class ItemManager {
     }
 
     public List<Item> getAllByUserId(int userId) {
-        String sql = "SELECT * FROM item where user_id=" + userId;
-        List<Item> items = new ArrayList<>();
-        List<Item> lastItems = new ArrayList<>();
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ResultSet resultSet = ps.executeQuery();
-            while (resultSet.next()) {
-                Item item = getItemFromResultSet(resultSet);
-                items.add(item);
-            }
-            if (items != null && !items.isEmpty() && items.size() >= 20) {
-                for (int i = 20; i > 0; i--) {
-                    Item lastItem = items.get(items.size() - i);
-                    lastItems.add(lastItem);
-                }
-            } else {
-                for (int i = 0; i < items.size(); i++) {
-                    Item lastItem = items.get(i);
-                    lastItems.add(lastItem);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return lastItems;
+        String sql = "select * from item  where user_id = ? order by id DESC limit 20";
+        return getItems(userId, sql);
     }
 
     public List<Item> getAllByCategoryId(int categoryId) {
-        String sql = "SELECT * FROM item where category_id=" + categoryId;
-        List<Item> items = new ArrayList<>();
-        List<Item> lastItems = new ArrayList<>();
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ResultSet resultSet = ps.executeQuery();
-            while (resultSet.next()) {
-                Item item = getItemFromResultSet(resultSet);
-                items.add(item);
-            }
-            if (items != null && !items.isEmpty() && items.size() >= 20) {
-                for (int i = 20; i > 0; i--) {
-                    Item lastItem = items.get(items.size() - i);
-                    lastItems.add(lastItem);
-                }
-            } else {
-                for (int i = 0; i < items.size(); i++) {
-                    Item lastItem = items.get(i);
-                    lastItems.add(lastItem);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return lastItems;
-    }
-
-    private Item getItemFromResultSet(ResultSet resultSet) throws SQLException {
-        Category category = categoryManager.getById(resultSet.getInt("category_id"));
-        User user = userManager.getById(resultSet.getInt("user_id"));
-        return Item.builder()
-                .id(resultSet.getInt("id"))
-                .title(resultSet.getString("title"))
-                .price(resultSet.getDouble("price"))
-                .category(category)
-                .picUrl(resultSet.getString("pic_url"))
-                .user(user)
-                .build();
+        String sql = "SELECT * FROM item where category_id = ? order by id desc limit 20";
+        return getItems(categoryId, sql);
     }
 
     public void deleteItemById(int id) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("delete from item where id =?");
+        String sql = "delete from item where id =?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -144,8 +81,7 @@ public class ItemManager {
 
     public void edit(Item item) {
         String sql = "update item set title=?,price=?,category_id=?,pic_url=? where id=?";
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, item.getTitle());
             ps.setDouble(2, item.getPrice());
             ps.setInt(3, item.getCategory().getId());
@@ -155,5 +91,31 @@ public class ItemManager {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private List<Item> getItems(int someId, String sql) {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, someId);
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                Item item = getItemFromResultSet(resultSet);
+                items.add(item);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    private Item getItemFromResultSet(ResultSet resultSet) throws SQLException {
+        return Item.builder()
+                .id(resultSet.getInt("id"))
+                .title(resultSet.getString("title"))
+                .price(resultSet.getDouble("price"))
+                .category(categoryManager.getById(resultSet.getInt("category_id")))
+                .picUrl(resultSet.getString("pic_url"))
+                .user(userManager.getById(resultSet.getInt("user_id")))
+                .build();
     }
 }
