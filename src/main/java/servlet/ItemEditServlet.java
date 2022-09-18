ceea71677e492c8f8658769b94a99e3dbe5315ec
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-18 22:11:35 +0400

    ItemEditServlet

--- src/main/java/servlet/ItemEditServlet.java
+++ src/main/java/servlet/ItemEditServlet.java
@@ -2,10 +2,7 @@ package servlet;
 
 import manager.CategoryManager;
 import manager.ItemManager;
-import manager.UserManager;
-import model.Category;
 import model.Item;
-import model.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,40 +13,35 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = "/items/edit")
 public class ItemEditServlet extends HttpServlet {
-    private ItemManager itemManager = new ItemManager();
-    private CategoryManager categoryManager = new CategoryManager();
-    private UserManager userManager = new UserManager();
+
+    private final ItemManager itemManager = new ItemManager();
+    private final CategoryManager categoryManager = new CategoryManager();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int itemId = Integer.parseInt(req.getParameter("id"));
-        Item item = itemManager.getById(itemId);
-        req.setAttribute("items", item);
+        Item item = itemManager.getById(Integer.parseInt(req.getParameter("id")));
+        req.setAttribute("item", item);
         req.setAttribute("categories", categoryManager.getAll());
-        req.setAttribute("users", userManager.getAll());
         req.getRequestDispatcher("/WEB-INF/editItem.jsp").forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         int itemId = Integer.parseInt(req.getParameter("id"));
         String title = req.getParameter("title");
         double price = Double.parseDouble(req.getParameter("price"));
-        int categoryId = Integer.parseInt(req.getParameter("category_id"));
         String picUrl = req.getParameter("pic_url");
-        Category category = categoryManager.getById(categoryId);
-//        int userId = Integer.parseInt(req.getParameter("user_id"));
-//        User user = userManager.getById(userId);
 
         Item item = Item.builder()
                 .title(title)
                 .price(price)
-                .category(category)
+                .category(categoryManager.getById(Integer.parseInt(req.getParameter("category_id"))))
                 .picUrl(picUrl)
-//                .user(user)
                 .id(itemId)
                 .build();
+
         itemManager.edit(item);
+
         resp.sendRedirect("/item");
     }
 }
